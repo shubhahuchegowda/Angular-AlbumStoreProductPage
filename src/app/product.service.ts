f4f093cc84692b27d85bc60351b192a30d346697
shubhahuchegowda <42028975+shubhahuchegowda@users.noreply.github.com>
2018-08-05 21:58:27 +0530

    corrected

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,6 +11,6 @@ export class ProductService {
    
     getAlbum(id:number)
     {
-      return this._http.get(this._albumUrl).map(response => response.json);
+      return this._http.get(this._albumUrl).map((response) => response.json());
     }
    }
