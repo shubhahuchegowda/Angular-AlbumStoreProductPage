83fbad0a66082bf2de7ec34e783f8efe2f3d57a2
shubhahuchegowda <42028975+shubhahuchegowda@users.noreply.github.com>
2018-08-05 13:28:50 +0530

    Product Service

---
+++ src/app/product.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { getLocaleNumberSymbol } from '../../node_modules/@angular/common';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class ProductService {
+
+  private _albumURL = '../assets/album.json';
+  constructor( private _http:Http) {}
+   
+    getAlbum(id:number)
+    {
+      return this._http.get(this._albumURL).map(response => response.json);
+    }
+   }
